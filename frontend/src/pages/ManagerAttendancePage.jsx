6b5d5a1f11fd3b693579a678d33fbf21655756d9
Alexchiper1 <alexchiper20@gmail.com>
2026-04-30 15:54:21 +0100

    Fixed

--- frontend/src/pages/ManagerAttendancePage.jsx
+++ frontend/src/pages/ManagerAttendancePage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "../css/ManagerOverviewPage.css";
 import "../css/ManagerProfilePage.css";
 import "../css/ManagerAttendancePage.css";
